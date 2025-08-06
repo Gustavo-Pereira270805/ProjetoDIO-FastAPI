4f73b203a453be44e71089991b47b29bdfe0a321
Gustavo Pereira <gdnp@academico.ufpb.br>
2025-08-06 10:36:51 -0300

    setup de pastas e configuração inicial do FASTAPI

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI(title = "ProjetoDIO")
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello World"}
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
